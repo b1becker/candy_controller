e4573d3bb662a78b793dea0a387c41de07d20523
b1becker <128233074+b1becker@users.noreply.github.com>
2025-04-21 17:26:21 -0400

    Some pulse works

--- src/main.c
+++ src/main.c
@@ -3,7 +3,11 @@
 #include <stdio.h>
 #include <string.h>
 
-unsigned int counter;
+#define Echo A0
+#define Trig A1
+
+volatile unsigned int counter;
+
 
 void SysTick_Handler(void)
 {
@@ -15,10 +19,10 @@ void SysTick_initialize(void)
     SysTick->CTRL = 0;  //Enables the SysTick timer
     // Reload value can be anything in the range 0x00000001-0x00FFFFFF. Setting
     // it to 4000 gives us a frequency of 1kHz.
-    SysTick->LOAD = 399; // Sets the SysTick timer's reload value
+    SysTick->LOAD = 39; // Sets the SysTick timer's reload value
 
     //3999 - 1000 us
-    //399  - 100  us  (ten of these is 10 us, which is what we need to send out)
+    //399  - 100  us  
     //39   - 10   us  
 
     // This sets the priority of the interrupt to 15 (2^4 - 1), which is the
@@ -30,6 +34,11 @@ void SysTick_initialize(void)
     SysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;
 }
 
+void delay(int ds) {
+    counter = 0;
+    while (counter != ds) {}
+}
+
 int main()
 {
     //Initalize Serial Communication and SysTick
@@ -37,10 +46,36 @@ int main()
     SysTick_initialize();
     char buffer[200];
 
-    //Measure and print
+    //Configure the Trig & Echo
+    // Echo - A0
+    // Trig - A1
+    gpio_config_mode(Echo, INPUT);
+    gpio_config_mode(Trig, OUTPUT);
+    volatile echo_signal;
+    // Do Stuff
     while(1) {
-        //Print
-        sprintf(buffer, "meow");
+        //Send out 10 microsecond signal
+        gpio_write(Trig, 1);
+        delay(1);
+        gpio_write(Trig, 0);
+
+        while (gpio_read(Echo) == 0);
+        //Read from echo
+        volatile int start_time = counter;
+        volatile int useless_int = 0;
+        while (gpio_read(Echo) != 0);
+        volatile int end_time = counter;
+
+        volatile int pulse_time = end_time - start_time;
+        volatile float distance = (pulse_time * 0.034) / 2;
+        
+        sprintf(buffer, "Useless int: %d\n", useless_int);
         serial_write(USART2, buffer, strlen(buffer));
+        sprintf(buffer, "Distance: %d\n", pulse_time);
+        serial_write(USART2, buffer, strlen(buffer));
+
+        delay(10000);
     }
 }
+
+// https://howtomechatronics.com/tutorials/arduino/ultrasonic-sensor-hc-sr04/
