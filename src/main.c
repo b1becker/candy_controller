023314a05741f3be5957642e66cd627091e9a391
b1becker <128233074+b1becker@users.noreply.github.com>
2025-04-25 14:42:43 -0400

    Pause/play functions enabled and ready to integrate with distance sensor

--- src/main.c
+++ src/main.c
@@ -11,109 +11,49 @@
 /* Pin Definitions: */
 #define Echo A0
 #define Trig A1
-// #define TX_UART D1
-// #define RX_UART D0 currently hard coded will use later
+
+/* Command Definitions */
+#define PLAY_NEXT 1
+#define PLAY_PREV 2
+#define DEFAULT 3
 
 /* Functions Declarations: */
 void SysTick_Handler(void);
 void SysTick_initialize(void);
-void delay_ms(int delay);
+void delay_us(int delay);
+void usart_send_command(uint8_t cmd);
+void usart_select_sd(void);
 
 
 /* Global Variable Declarations: */
 
-
-
-
-// void uart2_init(void) {
-//     // Enable GPIOA and USART2 clocks
-//     RCC->AHB2ENR  |= RCC_AHB2ENR_GPIOAEN;
-//     RCC->APB1ENR1 |= RCC_APB1ENR1_USART2EN;
-
-
-// }
-void usart_select_sd(void) {
-    const uint8_t buf[10] = {
-        0x7E,       // Start
-        0xFF,       // Version
-        0x06,       // Length
-        0x09,       // CMD = select device
-        0x00,       // No feedback
-        0x00, 0x02, // Param = 0x0002 (SD card)
-        0xFE, 0xF0, // Checksum (0xFEF0)
-        0xEF        // End
-    };
-    serial_write(USART1, (char*)buf, 10);
-}
-
-void usart_send_command(uint8_t cmd, uint16_t argument) {
-    
-    if(cmd == 3) {
-        const uint8_t write_buffer[10] = {
-            0x7E, 0xFF, 0x06, 0x03,
-            0x00, 0x00, 0x01,
-            0xFE, 0xF7, 0xEF
-        };
-        serial_write(USART1, (char*)write_buffer, 10);
-    }
-    
-}
-
+/* Main */
 int main() {
-
-
     //Initalize Serial Communication and SysTick
     host_serial_init();
     SysTick_initialize();
     // char buffer[200];
     
     usart_select_sd();
-    delay_ms(100000);
+    delay_us(100000);
+    
     /* Trig & Echo Config */
-    // gpio_config_mode(Echo, INPUT);
-    // gpio_config_mode(Trig, OUTPUT);
+    gpio_config_mode(Echo, INPUT);
+    gpio_config_mode(Trig, OUTPUT);
     
-    // const uint8_t write_buffer[10] = {
-        //     0x7E, 0xFF, 0x06, 0x03,
-        //     0x00, 0x00, 0x01,
-        //     0xFE, 0xF7, 0xEF
-        // };
-        // serial_write(USART2, (char*)write_buffer, 10);
-        
-        // gpio_config_mode(A3, OUTPUT);
-    usart_send_command(3, 0);
-        while (1)
-        {
-            
-            // serial_write(USART2, buffer, strlen(buffer));
-            delay_ms(1000000);
-        
-            delay_ms(100000);
+    usart_send_command(DEFAULT);
+    while (1)
+    {
+        // serial_write(USART2, buffer, strlen(buffer));
+        delay_us(1000000);
+        usart_send_command(PLAY_NEXT);
+        // delay_us(100000);
+        // usart_send_command(PLAY_NEXT);
             // usart_send_command(3, 0);
-    }
-    
-}
 
-// leave this for later
-// int main()
-// {
-//     //Initalize Serial Communication and SysTick
-//     host_serial_init();
-//     SysTick_initialize();
-//     char buffer[200];
-
-//     /* Trig & Echo Config */
-//     gpio_config_mode(Echo, INPUT);
-//     gpio_config_mode(Trig, OUTPUT);
-
-
-//     gpio_config_mode(A3, OUTPUT);
-
-
-//     while(1) {
 //         //Send out 10 microsecond signal
 //         gpio_write(Trig, 1);
-//         delay_ms(1);
+//         delay_us(1);
 //         gpio_write(Trig, 0);
 
 //         while (gpio_read(Echo) == 0);
@@ -130,14 +70,10 @@ int main() {
 //         sprintf(buffer, "Distance: %f\n", distance);
 //         serial_write(USART2, buffer, strlen(buffer));
         
-//         delay_ms(10000);
-//         if(distance < 10.0f) {
-//             gpio_write(A3, 1);            
-//         } else {
-//             gpio_write(A3, 0);
-//         }
-//     }
-// }
+//         delay_us(10000);
+
+    }
+}
 
 volatile unsigned int counter;
 void SysTick_Handler(void)
@@ -165,7 +101,7 @@ void SysTick_initialize(void)
     SysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;
 }
 
-void delay_ms(int ds) {
+void delay_us(int ds) {
     counter = 0;
     while (counter != ds) {}
 }
@@ -174,6 +110,43 @@ void delay_ms(int ds) {
 int _write(int file, char *data, int len) {
     serial_write(USART2, data, len);
     return len;
+}
+
+void usart_send_command(uint8_t cmd) {
+    // play very first track is default
+    uint8_t write_buffer[10] = {
+        0x7E, 0xFF, 0x06, 0x03,
+        0x00, 0x00, 0x01,
+        0xFE, 0xF7, 0xEF
+    };
+    // play next
+    if(cmd == 1) {
+        write_buffer[3] = cmd;
+        write_buffer[6] = 0x0;
+        write_buffer[8] = 0xfa;
+        serial_write(USART1, (const char*)write_buffer, 10);
+        delay_us(100000);
+    }
+    // play previous
+    else if(cmd == 2) {
+        write_buffer[3] = cmd;
+        write_buffer[8] = 0xf9;
+    }
+    serial_write(USART1, (const char*)write_buffer, 10);
+}
+
+void usart_select_sd(void) {
+    const uint8_t buf[10] = {
+        0x7E,       // Start
+        0xFF,       // Version
+        0x06,       // Length
+        0x09,       // CMD = select device
+        0x00,       // No feedback
+        0x00, 0x02, // Param = 0x0002 (SD card)
+        0xFE, 0xF0, // Checksum (0xFEF0)
+        0xEF        // End
+    };
+    serial_write(USART1, (char*)buf, 10);
 }
 
 // https://howtomechatronics.com/tutorials/arduino/ultrasonic-sensor-hc-sr04/
